facb1a5c25873fce398172effd816191edbbfb55
MajaDit <marie.ditrtova@gmail.com>
2021-08-02 15:00:19 +0100

    updated Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,14 +4,15 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
+      <br />
       <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map((definition, index) => {
         return (
           <div key={index}>
-            <br />
+            <strong>Definition: </strong>
             {definition.definition}
             <br />
-            <br />
+            <strong>Example: </strong>
             <em>{definition.example}</em>
           </div>
         );
