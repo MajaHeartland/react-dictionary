0e321abb12c8fa7ecac2822a2d309798c416642f
MajaDit <marie.ditrtova@gmail.com>
2022-08-03 12:49:21 +0100

    commented out phonetics audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,16 +4,16 @@ import "./Phonetics.css";
 export default function Phonetics(props) {
   if (props.phonetics) {
     return (
-      <div className="Phonetics">
-        <a
-          href={props.phonetics.audio}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fas fa-volume-up"></i>
-        </a>
-        <span> {props.phonetics.text}</span>
-      </div>
+      // <div className="Phonetics">
+      //   <a
+      //     href={props.phonetics.audio}
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     <i className="fas fa-volume-up"></i>
+      //   </a>
+      <span> {props.phonetics.text}</span>
+      // </div>
     );
   } else {
     return null;
